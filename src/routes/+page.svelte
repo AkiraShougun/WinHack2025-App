eb042953e7065bb869d52774b214bb10129065a1
Kensukeken <hiaxsaleh@gmail.com>
2025-02-15 14:30:42 -0500

    CSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<ol>
-    <li><a href="/grocery">Grocery</a></li>
-    <li><a href="/trivia">Trivia</a></li>
-    <li><a href="/wheel">Wheel</a></li>
-</ol>
+<link rel="stylesheet" href="/src/routes/css/main.css" />
+
+<header>
+    <h1>Welcome to SvelteKit</h1>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/grocery">Grocery</a>
+        <a href="/trivia">Trivia</a>
+        <a href="/wheel">Wheel</a>
+    </nav>
+</header>
+
+<main>
+    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+    <ol>
+        <li><a href="/grocery">Grocery</a></li>
+        <li><a href="/trivia">Trivia</a></li>
+        <li><a href="/wheel">Wheel</a></li>
+    </ol>
+</main>
+
+<footer>
+    <p>&copy; 2025 WinHack2025-App. </p>
+</footer>
