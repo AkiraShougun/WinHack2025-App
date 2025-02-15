867b37a581cc97b2672977c6d2921a901afa95ec
AkiraShougun <153398075+AkiraShougun@users.noreply.github.com>
2025-02-15 12:12:22 -0500

    routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<ol>
+    <li><a href="/grocery">Grocery</a></li>
+    <li><a href="/trivia">Trivia</a></li>
+    <li><a href="/wheel">Wheel</a></li>
+</ol>
