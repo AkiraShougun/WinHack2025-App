d1ceeb587f3275fb94297e07da48f3eb6c0bb4d3
AkiraShougun <153398075+AkiraShougun@users.noreply.github.com>
2025-02-16 09:32:41 -0500

    idk lmao

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
+<script>
+    import {goto} from '$app/navigation';
+</script>
+
 <div>
     <div class="features">
         <h2 class="section-title">Explore Our Features</h2>
-        <ul class="features-list">
-            <li><a href="/grocery">Grocery</a></li>
-            <li><a href="/trivia">Trivia</a></li>
-            <li><a href="/wheel">Wheel</a></li>
-        </ul>
+            <button onclick={() => goto('/grocery')}>Grocery</button>
+            <button onclick={() => goto('/trivia')}>Trivia</button>
+            <button onclick={() => goto('/wheel')}>Wheel</button>
     </div>
 </div>
